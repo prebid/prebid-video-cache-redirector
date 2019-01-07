cc6e2432960e14c4f605fe9c39943f7f7d57b731
Sergii Chernysh <jetboy2002@gmail.com>
2019-01-07 14:48:37 +0200

    Initial implementation with /redir endpoint

---
+++ src/main/java/org/prebid/video/cache/redirector/HttpServerVerticle.java
@@ -0,0 +1,73 @@
+package org.prebid.video.cache.redirector;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HttpServerVerticle extends AbstractVerticle {
+
+    private static final String REDIR_ENDPOINT = "/redir";
+    private static final String HOST_QUERY_PARAM = "host";
+    private static final String UUID_QUERY_PARAM = "uuid";
+
+    private static final String UUID_PLACEHOLDER = "{{uuid}}";
+
+    private Map<String, String> mapping;
+
+    @Override
+    public void start(Future<Void> future) {
+        final int port = config().getInteger("http.port");
+        mapping = config().getJsonObject("redirect.mapping")
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().toString()));
+
+        final Router router = Router.router(vertx);
+
+        router.get(REDIR_ENDPOINT).handler(this::handleRedir);
+
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(port, result -> {
+                    if (result.succeeded()) {
+                        future.complete();
+                    } else {
+                        future.fail(result.cause());
+                    }
+                });
+    }
+
+    private void handleRedir(RoutingContext context) {
+        final List<String> hostValues = context.queryParam(HOST_QUERY_PARAM);
+        final List<String> uuidValues = context.queryParam(UUID_QUERY_PARAM);
+
+        if (hostValues.isEmpty() || hostValues.size() > 1 || uuidValues.isEmpty() || uuidValues.size() > 1) {
+            context.response()
+                    .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+                    .end("Request should contain single host and uuid query parameters");
+            return;
+        }
+
+        final String host = hostValues.get(0);
+
+        if (!mapping.containsKey(host)) {
+            context.response()
+                    .setStatusCode(HttpResponseStatus.NOT_FOUND.code())
+                    .end("Unknown host");
+            return;
+        }
+
+        final String location = mapping.get(host).replace(UUID_PLACEHOLDER, uuidValues.get(0));
+
+        context.response()
+                .setStatusCode(302)
+                .putHeader(HttpHeaders.LOCATION, location)
+                .end();
+    }
+}
---
+++ src/test/java/org/prebid/video/cache/redirector/HttpServerVerticleTest.java
@@ -0,0 +1,94 @@
+package org.prebid.video.cache.redirector;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(VertxExtension.class)
+public class HttpServerVerticleTest {
+
+    private static final int PORT = 8080;
+    private static final Map<String, String> REDIRECT_MAPPING = Collections.singletonMap(
+            "prebid-server.rubiconproject.com", "https://prebid-server.rubiconproject.com/cache?uuid={{uuid}}");
+
+    private static WebClient webClient;
+
+    @BeforeAll
+    @DisplayName("Deploy a verticle and initialize web client")
+    public static void setUp(Vertx vertx, VertxTestContext testContext) {
+
+        final DeploymentOptions options = new DeploymentOptions()
+                .setConfig(new JsonObject()
+                        .put("http.port", PORT)
+                        .put("redirect.mapping", REDIRECT_MAPPING));
+
+        vertx.deployVerticle(new HttpServerVerticle(), options, testContext.completing());
+        webClient = WebClient.create(vertx, new WebClientOptions().setFollowRedirects(false));
+    }
+
+    @Test
+    @DisplayName("Positive: known host should result in 302 with location header")
+    public void shouldRespondWith302AndLocation(VertxTestContext testContext) {
+        webClient.get(8080, "localhost", "/redir")
+                .addQueryParam("host", "prebid-server.rubiconproject.com")
+                .addQueryParam("uuid", "123")
+                .send(testContext.succeeding(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), 302);
+                    assertEquals(response.getHeader("Location"),
+                            "https://prebid-server.rubiconproject.com/cache?uuid=123");
+                    testContext.completeNow();
+                })));
+    }
+
+    @Test
+    @DisplayName("Negative: unknown host should result in 404")
+    public void shouldRespondWith404IfHostUnknown(VertxTestContext testContext) {
+        webClient.get(8080, "localhost", "/redir")
+                .addQueryParam("host", "prebid.example.com")
+                .addQueryParam("uuid", "123")
+                .send(testContext.succeeding(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), 404);
+                    assertEquals(response.bodyAsString(), "Unknown host");
+                    testContext.completeNow();
+                })));
+    }
+
+    @Test
+    @DisplayName("Negative: missing uuid should result in 400")
+    public void shouldRespondWith400IfUuidMissing(VertxTestContext testContext) {
+        webClient.get(8080, "localhost", "/redir")
+                .addQueryParam("host", "prebid-server.rubiconproject.com")
+                .send(testContext.succeeding(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), 400);
+                    assertEquals(response.bodyAsString(),
+                            "Request should contain single host and uuid query parameters");
+                    testContext.completeNow();
+                })));
+    }
+
+    @Test
+    @DisplayName("Negative: missing host should result in 400")
+    public void shouldRespondWith400IfHostMissing(VertxTestContext testContext) {
+        webClient.get(8080, "localhost", "/redir")
+                .addQueryParam("uuid", "123")
+                .send(testContext.succeeding(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), 400);
+                    assertEquals(response.bodyAsString(),
+                            "Request should contain single host and uuid query parameters");
+                    testContext.completeNow();
+                })));
+    }
+}
